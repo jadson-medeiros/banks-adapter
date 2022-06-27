7e4c218a9b9979d52a947a6dd7d568044e52a146
Jadson Medeiros <developer.medeiros@gmail.com>
2022-06-27 00:29:14 -0300

    add adapters

---
+++ adapters/bank1_adapter.go
@@ -0,0 +1,26 @@
+package adapters
+
+import (
+	"time"
+
+	"github.com/jadson-medeiros/banks-adapter/bank1"
+	"github.com/jadson-medeiros/banks-adapter/domain"
+)
+
+type Bank1Adapter struct{}
+
+func (b *Bank1Adapter) GetBalance(accountID int64) float64 {
+	return bank1.GetAccountBalance(accountID)
+}
+
+func (b *Bank1Adapter) GetCurrency(accountID int64) string {
+	return bank1.GetAccountCurrency(accountID)
+}
+
+func (b *Bank1Adapter) GetTransactions(account int64, fromDate time.Time, toDate time.Time) []domain.Transaction {
+	checkDeposit := domain.Transaction{Amount: 100, Type: domain.CREDIT, Text: "Check deposit"}
+	debidCardPurchase := domain.Transaction{Amount: 25.5, Type: domain.DEBIT, Text: "Debit card purchase"}
+	rentPayment := domain.Transaction{Amount: 225, Type: domain.DEBIT, Text: "Rent payment"}
+
+	return []domain.Transaction{checkDeposit, debidCardPurchase, rentPayment}
+}
---
+++ adapters/bank2_adapter.go
@@ -0,0 +1,28 @@
+package adapters
+
+import (
+	"time"
+
+	"github.com/jadson-medeiros/banks-adapter/bank2"
+	"github.com/jadson-medeiros/banks-adapter/domain"
+)
+
+type Bank2Adapter struct{}
+
+func (b *Bank2Adapter) GetBalance(accountID int64) float64 {
+	balance := bank2.GetBalance(accountID)
+	return balance.Balance
+}
+
+func (b *Bank2Adapter) GetCurrency(accountID int64) string {
+	balance := bank2.GetBalance(accountID)
+	return balance.Currency
+}
+
+func (b *Bank2Adapter) GetTransactions(account int64, fromDate time.Time, toDate time.Time) []domain.Transaction {
+	purchase := domain.Transaction{Amount: 125, Type: domain.DEBIT, Text: "Amazon.com"}
+	carInsurance := domain.Transaction{Amount: 500, Type: domain.DEBIT, Text: "Car insurance"}
+	salary := domain.Transaction{Amount: 800, Type: domain.CREDIT, Text: "Salary"}
+
+	return []domain.Transaction{purchase, carInsurance, salary}
+}
