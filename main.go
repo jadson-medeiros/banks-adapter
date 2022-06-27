a69e9dcfd62d1da8dfd9253bb1ce95d10f3eea93
Jadson Medeiros <developer.medeiros@gmail.com>
2022-06-27 00:29:40 -0300

    added main with interface

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/jadson-medeiros/banks-adapter/adapters"
+	"github.com/jadson-medeiros/banks-adapter/domain"
+)
+
+type BanksMethod interface {
+	GetTransactions(account int64, fromDate time.Time, toDate time.Time) []domain.Transaction
+	GetBalance(accountId int64) float64
+	GetCurrency(accountId int64) string
+}
+
+type Bank struct {
+	Method BanksMethod
+}
 
 func main() {
+
+	bank1 := &Bank{
+		Method: &adapters.Bank1Adapter{},
+	}
+
+	bank2 := &Bank{
+		Method: &adapters.Bank2Adapter{},
+	}
+
+	bank1.Method.GetTransactions(100, time.Now(), time.Now())
+
 	fmt.Println("Starting")
 }
