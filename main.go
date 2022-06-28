4db42f53ccbcefa4c326b24c2477e83d14828613
Jadson Medeiros <developer.medeiros@gmail.com>
2022-06-28 16:16:25 -0300

    aplied execution of the prints that will show the transactions and balances

--- main.go
+++ main.go
@@ -13,24 +13,34 @@ type Bank struct {
 }
 
 func main() {
-
 	bank1 := &Bank{
 		Method: &adapters.Bank1Adapter{},
 	}
 
-	// bank2 := &Bank{
-	// 	Method: &adapters.Bank2Adapter{},
-	// }
+	bank2 := &Bank{
+		Method: &adapters.Bank2Adapter{},
+	}
 
-	bank1.Method.GetTransactions(100, time.Now(), time.Now())
+	transactionsBank1 := bank1.Method.GetTransactions(100, time.Now(), time.Now())
+	transactionsBank2 := bank2.Method.GetTransactions(100, time.Now(), time.Now())
 
-	fmt.Println("Starting")
+	banksTransactions := append([]domain.Transaction{}, transactionsBank1...)
+
+	banksTransactions = append(banksTransactions, transactionsBank2...)
+
+	var banks []Bank
+	banks = append(banks, *bank1, *bank2)
+
+	PrintBalances(banks)
+	PrintTransactions(banksTransactions)
 }
 
-func PrintBalances() {
-	fmt.Println("Implement me to pull balance information from all available bank integrations and display them, one after the other.")
+func PrintBalances(banks []Bank) {
+	for _, value := range banks {
+		fmt.Printf("Balance: %v\n", value.Method.GetBalance(100))
+	}
 }
 
-func PrintTransactions() {
-	fmt.Println("Implement me to pull transactions from all available bank integrations and display them, one after the other.")
+func PrintTransactions(banks []domain.Transaction) {
+	fmt.Printf("Transactions: %v\n", banks)
 }
